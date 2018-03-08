0284a4d2a934da07a883111ce5507d9d3c96f86c
cravler <sergeizz@gmail.com>
2018-03-08 08:27:22 +0200

    refactoring to async/await

--- index.js
+++ index.js
@@ -1,150 +1,147 @@
 'use strict';
 
-var fs = require('fs');
-var mkdirp = require('mkdirp');
-var console = require('x-console');
+const fs = require('fs');
+const util = require('util');
+const mkdirp = util.promisify(require('mkdirp'));
+const fsWriteFile = util.promisify(fs.writeFile);
 
 module.exports = exports;
 
 /**
  * @param whaler
  */
-function exports(whaler) {
+async function exports (whaler) {
 
-    whaler.after('start', function* (options) {
-        yield touchHosts.$call(null, whaler);
+    whaler.after('start', async ctx => {
+        await touchHosts();
     });
 
-    whaler.after('stop', function* (options) {
-        yield touchHosts.$call(null, whaler);
+    whaler.after('stop', async ctx => {
+        await touchHosts();
     });
 
-    whaler.after('remove', function* (options) {
-        yield touchHosts.$call(null, whaler);
+    whaler.after('remove', async ctx => {
+        await touchHosts();
     });
 
-}
+    // PRIVATE
 
-// PRIVATE
+    const touchHosts = async () => {
+        const { default: docker } = await whaler.fetch('docker');
+        const { default: storage } = await whaler.fetch('apps');
+        const apps = await storage.all();
 
-/**
- * @param whaler
- */
-function* touchHosts(whaler) {
-    const docker = whaler.get('docker');
-    const storage = whaler.get('apps');
-    const apps = yield storage.all.$call(storage);
+        const hosts = [];
+        const domain = process.env.WHALER_HOSTS_PLUGIN_DOMAIN || 'whaler.lh';
 
-    const hosts = [];
-    const domain = process.env.WHALER_HOSTS_PLUGIN_DOMAIN || 'whaler.lh';
+        for (let appName in apps) {
+            const containers = await docker.listContainers({
+                all: true,
+                filters: JSON.stringify({
+                    name: [
+                        docker.util.nameFilter(appName)
+                    ]
+                })
+            });
 
-    for (let appName in apps) {
-        const containers = yield docker.listContainers.$call(docker, {
-            all: true,
-            filters: JSON.stringify({
-                name: [
-                    docker.util.nameFilter(appName)
-                ]
-            })
-        });
+            for (let info of containers) {
+                if ('running' == info['State']) {
+                    const parts = info['Names'][0].substr(1).split('.');
+                    hosts.push({
+                        ip: info['NetworkSettings']['Networks']['bridge']['IPAddress'],
+                        domain: parts[0] + '.' + appName + '.' + domain
+                    });
+                }
+            }
+        }
 
-        for (let info of containers) {
-            if ('running' == info['State']) {
-                const parts = info['Names'][0].substr(1).split('.');
-                hosts.push({
-                    ip: info['NetworkSettings']['Networks']['bridge']['IPAddress'],
-                    domain: parts[0] + '.' + appName + '.' + domain
+        await mkdirp('/var/lib/whaler/plugins/dnsmasq');
+
+        let res = [];
+        for (let data of hosts) {
+            res.push(data['ip'] + '\t' + data['domain']);
+        }
+        res.push('');
+        await fsWriteFile('/var/lib/whaler/plugins/dnsmasq/hosts', res.join('\n'));
+
+        let dockerBridgeNetwork = docker.getNetwork('bridge');
+        const bridgeInfo = await dockerBridgeNetwork.inspect();
+
+        let conf = [
+            'no-resolv',
+            'no-hosts',
+            'addn-hosts=/etc/dnsmasq.hosts',
+            'address=/' + domain + '/' + bridgeInfo['IPAM']['Config'][0]['Gateway'],
+            ''
+        ];
+        await fsWriteFile('/var/lib/whaler/plugins/dnsmasq/conf', conf.join('\n'));
+
+        let created = false;
+        let started = false;
+
+        let container = docker.getContainer('whaler_hosts');
+        try {
+            const info = await container.inspect();
+            created = true;
+            if (info['State']['Running']) {
+                started = true;
+            }
+        } catch (e) {}
+
+        if (!created) {
+            try {
+                await docker.followPull('andyshinn/dnsmasq:2.76');
+            } catch(e) {}
+
+            let whalerHostsNetwork = null;
+            try {
+                whalerHostsNetwork = await docker.createNetwork({
+                    'Name': 'whaler_hosts_nw',
+                    'CheckDuplicate': true
+                });
+            } catch (e) {
+                whalerHostsNetwork = docker.getNetwork('whaler_hosts_nw');
+            }
+
+            const createOpts = {
+                'name': 'whaler_hosts',
+                'Image': 'andyshinn/dnsmasq:2.76',
+                'Entrypoint': [],
+                'Cmd': ['dnsmasq', '--no-daemon'],
+                'HostConfig': {
+                    'Binds': [
+                        '/var/lib/whaler/plugins/dnsmasq/hosts:/etc/dnsmasq.hosts',
+                        '/var/lib/whaler/plugins/dnsmasq/conf:/etc/dnsmasq.conf'
+                    ],
+                    'RestartPolicy': {
+                        'Name': 'always'
+                    }
+                }
+            };
+
+            container = await docker.createContainer(createOpts);
+
+            if (whalerHostsNetwork) {
+                const nwInfo = await whalerHostsNetwork.inspect();
+
+                await whalerHostsNetwork.connect({
+                    'Container': container.id
                 });
             }
         }
-    }
 
-    yield mkdirp.$call(null, '/var/lib/whaler/plugins/dnsmasq');
+        if (started) {
+            await container.restart();
 
-    let res = [];
-    for (let data of hosts) {
-        res.push(data['ip'] + '\t' + data['domain']);
-    }
-    res.push('');
-    yield fs.writeFile.$call(null, '/var/lib/whaler/plugins/dnsmasq/hosts', res.join('\n'));
+        } else {
+            await container.start();
+        }
 
-    let dockerBridgeNetwork = docker.getNetwork('bridge');
-    const bridgeInfo = yield dockerBridgeNetwork.inspect.$call(dockerBridgeNetwork);
-
-    let conf = [
-        'no-resolv',
-        'no-hosts',
-        'addn-hosts=/etc/dnsmasq.hosts',
-        'address=/' + domain + '/' + bridgeInfo['IPAM']['Config'][0]['Gateway'],
-        ''
-    ];
-    yield fs.writeFile.$call(null, '/var/lib/whaler/plugins/dnsmasq/conf', conf.join('\n'));
-
-    let created = false;
-    let started = false;
-
-    let container = docker.getContainer('whaler_hosts');
-    try {
-        const info = yield container.inspect.$call(container);
-        created = true;
+        const info = await container.inspect();
         if (info['State']['Running']) {
-            started = true;
+            const ip = info['NetworkSettings']['Networks']['whaler_hosts_nw']['IPAddress'];
+            whaler.info('Hosts server %s. IP:', started ? 'restarted' : 'started', ip);
         }
-    } catch (e) {}
+    };
 
-    if (!created) {
-        try {
-            yield docker.followPull.$call(docker, 'andyshinn/dnsmasq:2.76');
-        } catch(e) {}
-
-        let whalerHostsNetwork = null;
-        try {
-            whalerHostsNetwork = yield docker.createNetwork.$call(docker, {
-                'Name': 'whaler_hosts_nw',
-                'CheckDuplicate': true
-            });
-        } catch (e) {
-            whalerHostsNetwork = docker.getNetwork('whaler_hosts_nw');
-        }
-
-        const createOpts = {
-            'name': 'whaler_hosts',
-            'Image': 'andyshinn/dnsmasq:2.76',
-            'Entrypoint': [],
-            'Cmd': ['dnsmasq', '--no-daemon'],
-            'HostConfig': {
-                'Binds': [
-                    '/var/lib/whaler/plugins/dnsmasq/hosts:/etc/dnsmasq.hosts',
-                    '/var/lib/whaler/plugins/dnsmasq/conf:/etc/dnsmasq.conf'
-                ],
-                'RestartPolicy': {
-                    'Name': 'always'
-                }
-            }
-        };
-
-        container = yield docker.createContainer.$call(docker, createOpts);
-
-        if (whalerHostsNetwork) {
-            const nwInfo = yield whalerHostsNetwork.inspect.$call(whalerHostsNetwork);
-
-            yield whalerHostsNetwork.connect.$call(whalerHostsNetwork, {
-                'Container': container.id
-            });
-        }
-    }
-
-    if (started) {
-        yield container.restart.$call(container);
-
-    } else {
-        yield container.start.$call(container);
-    }
-
-    const info = yield container.inspect.$call(container);
-    if (info['State']['Running']) {
-        const ip = info['NetworkSettings']['Networks']['whaler_hosts_nw']['IPAddress'];
-        console.info('');
-        console.info('[%s] Hosts server %s. IP:', process.pid, started ? 'restarted' : 'started', ip);
-    }
 }
